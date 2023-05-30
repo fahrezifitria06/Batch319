bad6b2646b250fd25614a70d12c88f4620481bd8
fahrezifitria06 <fahrezifitria@gmail.com>
2023-05-30 13:36:45 +0700

    first commit

---
+++ Logic01/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Konversi();
+OperatorAritmatika();
+
+Console.ReadKey();
+
+static void OperatorAritmatika()
+{
+    int mangga, apel, hasil = 0;
+
+    Console.WriteLine("--Operator Aritmatika--");
+    Console.Write("Masukkan mangga : ");
+    mangga = int.Parse(Console.ReadLine());
+    Console.Write("Masukkan apel : ");
+    apel = int.Parse(Console.ReadLine());
+
+    hasil = mangga + apel;
+
+    string nama = "Petani" + 10;
+    int angka = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Hasil mangga + apel = {hasil}");
+
+}
+
+static void Konversi()
+{
+    Console.WriteLine("---KONVERSI---");
+
+    int myInt = 10;
+    double myDouble = 5.25;
+    bool myBool = true;
+
+    string strMyInt = Convert.ToString(myInt);
+    string strMyInt2 = myInt.ToString();
+
+    Console.WriteLine(strMyInt);
+    Console.WriteLine(strMyInt2);
+    Console.WriteLine(Convert.ToDouble(myInt));
+    Console.WriteLine(Convert.ToInt32(myDouble));
+    Console.WriteLine(Convert.ToString(myBool));
+
+}
---
+++ Logic319/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Guys!");
+Console.Write("Hello Guys");
+Console.Write("Hello Guts");
+Console.WriteLine("Hello");
+Console.WriteLine("Halo");
+
+Console.WriteLine("==Biodata Ezi==");
+Console.Write("Nama\t: ");
+Console.WriteLine("Ezoyy");
+//Console.WriteLine();
+Console.WriteLine("Alamat \t: Jakarta");
+
+Console.WriteLine();
+
+Console.WriteLine("== PROGRAM KEREN==");
+Console.Write("Tuliskan namamu :");
+string nama = Console.ReadLine();
+Console.WriteLine("Hai, {0} Selamat Datang", nama);
+Console.WriteLine("Hai, " + nama + " Selamat Datang");
+Console.WriteLine($"Hai, {nama} Selamat Datang");
+
+//implisit
+var namaWeb = "Petani Kode";
+var umur = 18;
+var isMenikah = false;
+Console.WriteLine("nama : " + nama);
+Console.WriteLine("umur : " + umur);
+Console.WriteLine("isMenikah : " + isMenikah);
+
+//immutable
+//const double phi = 3.14;
+
+
+Console.ReadKey();
